cd90340391f2e44af0bf87abb00e7060d4141604
Bruno Verçosa <bruno.vmartins@hotmail.com>
2025-07-17 03:11:10 -0300

    feat: implementar login automático no EAS utilizando Expect
    - O script eas_login.sh foi reescrito para utilizar Expect, permitindo um login automático no Expo Application Services (EAS) com credenciais definidas.
    - Adicionada lógica para realizar logout antes do login e exibir mensagens de sucesso ou falha, melhorando a experiência do usuário e a automação do processo.

--- templates/eas_login.sh
+++ templates/eas_login.sh
@@ -1,8 +1,38 @@
-#!/bin/bash
-#
-# Este script faz o login no Expo Application Services (EAS).
-# Ele utilizará a variável de ambiente EXPO_TOKEN se estiver definida (ideal para CI/CD).
-# Caso contrário, ele solicitará um login interativo.
-#
-echo "Tentando fazer login no EAS..."
-eas login
+#!/usr/bin/expect
+
+# Defina as variáveis de login e senha aqui
+set username "diegoadriian6@proton.me"
+set password "Linux321!"
+
+# Desativa o log para não mostrar as mensagens do spawn
+log_user 0
+
+# Executa o comando logout e verifica se foi bem-sucedido
+spawn sh -c "eas logout >/dev/null 2>&1"
+expect eof
+set logout_status $::spawn_id
+
+# Executa o comando login e verifica se foi bem-sucedido
+spawn sh -c "eas login >/dev/null 2>&1"
+expect "Email or username"
+send "$username\r"
+expect "Password"
+send "$password\r"
+set login_status $::spawn_id
+
+# Permite que o login seja concluído
+interact
+
+# Ativa novamente o log para exibir mensagens de status
+log_user 1
+
+# Exibe uma mensagem para indicar o sucesso ou falha do logout e login
+if {$logout_status != -1} {
+    if {$login_status != -1} {
+        puts "Login realizado com sucesso."
+    } else {
+        puts "Erro ao realizar login."
+    }
+} else {
+    puts "Erro ao realizar logout."
+}
